ef80092c80f96ab9279fe1053c4cd747bc2fa2e3
mixiaotian <654136696@qq.com>
2018-02-23 17:32:29 +0800

    add content

---
+++ main.js
@@ -0,0 +1,13 @@
+ 
+  $(document).ready(function(){  
+   //typed js
+    $(".typed").typed({
+        strings: ["I am Risky", "I'm a Web Designer", "The web is building","敬请期待..."],
+        typeSpeed: 100,
+        backDelay: 900,
+        // loop
+        loop: true
+    });
+   
+
+})(jQuery);
