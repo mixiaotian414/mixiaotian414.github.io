80eb748be897dd2d009410011a14529f03e87962
mixiaotian414 <33337918+mixiaotian414@users.noreply.github.com>
2022-03-30 16:03:03 +0800

    111
    1

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
   $(document).ready(function(){  
    //typed js
     $(".typed").typed({
-        strings: ["Baby Lzy", "I love three things in the world", "sun,moon and you", "sun for morning,moon for night and you forever."],
+        strings: ["Baby Lzy", "I love three things in the world", "sun,moon and you", "sun for morning,moon for night and you forever.", "浮世三千 吾爱有三", "日，月与卿", "日为朝", "月为暮", "卿为朝朝暮暮"],
         typeSpeed: 100,
         backDelay: 900,
         // loop
